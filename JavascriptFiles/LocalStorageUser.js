85f3f71485bab89af386a6738a9f5864afad5b60
Lamasusb000 <71854221+Lamasusb000@users.noreply.github.com>
2020-09-29 21:47:14 -0700

    Adding Usernames!

---
+++ JavascriptFiles/LocalStorageUser.js
@@ -0,0 +1,31 @@
+function ChangeUsername(){
+	console.log(document.getElementById("Username").value)
+	if (document.getElementById("Username").value == null | document.getElementById("Username").value == "null" | document.getElementById("Username").value == ""){
+		document.getElementById("NameOutput").innerHTML = "<p>Click The Button Below If You Wish To Delete Your Name<p>"
+	}else{
+		localStorage.setItem("User's Name", document.getElementById("Username").value)
+		document.getElementById("NameOutput").innerHTML = "<p>Username Has Been Changed!<p>"
+		SendToPage()
+	}
+}
+
+function SendToPage(){
+	if (localStorage.getItem("User's Name") == "null" | localStorage.getItem("User's Name") == null){
+		return
+	}else{
+		document.getElementById("Welcome").innerHTML = "Welcome " + localStorage.getItem("User's Name") + ", To Junior's Random Project Index"
+	}
+	setTimeout(ResetNameOutput, 3000)
+}
+
+function ResetNameOutput(){
+	document.getElementById("NameOutput").innerHTML = ""
+}
+function DeleteUsername(){
+	localStorage.removeItem("User's Name")
+	document.getElementById("NameOutput").innerHTML = "Your Username Has Been Deleted!"
+	setTimeout(Reload, 2000)
+}
+function Reload(){
+	location.reload()
+}
