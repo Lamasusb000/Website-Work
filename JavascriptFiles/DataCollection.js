c65e277d07bcaf3297bbf85c5ea276fc498dc9d7
Lamasusb000 <Usbaldolamasjr@gmail.com>
2020-11-06 11:23:00 -0800

    Update DataCollection.js
    Test Script

--- JavascriptFiles/DataCollection.js
+++ JavascriptFiles/DataCollection.js
@@ -173,3 +173,14 @@ function UserInfo(){
 		return ""
 	}
 }
+
+function DateConverter(Starter){
+	Starter = "2020-10-22T23:04:44Z"
+	var [Day, Month, Year, Hour, Minute] = [0,0,0,0,0]
+	Year = Starter.slice(0,4)
+	Month = Starter.slice(4, 6)
+	Day = Starter.slice(6, 8)
+	Hour = Starter.slice(8,10)
+	Minute = Starter.slice(10,12)
+	return `${Hour}:${Minute}  ${Month}/${Day}/${Year}`
+}
