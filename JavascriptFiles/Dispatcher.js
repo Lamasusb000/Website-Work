bfe3b9a30e2960dfe7c22f803644bcae2617c4f2
Lamasusb000 <Usbaldolamasjr@gmail.com>
2020-11-06 14:28:48 -0800

    Set Waves

--- JavascriptFiles/Dispatcher.js
+++ JavascriptFiles/Dispatcher.js
@@ -1,4 +1,35 @@
 function SetCount(){
     Waves = document.getElementById("WaveSelect").value
-    console.log(`${Waves} Waves`)
+    RouteList = document.getElementById("RouteList")
+    WaveList = []
+    WaveList.push(`
+    <h1>Wave Settings</h1>
+    <p>Resubmitting Erases Data</p>
+    `)
+    for (let i = 1; i <= Waves; i++) {
+        WaveList.push(`
+        <span>Wave ${i} DSP
+        <input type="text" id="DSP${i}"></span>
+        `)
+    }
+    WaveList.push(`
+    <input type="button" value="Set Route List" onclick="SetRouteData()">
+    `)
+    RouteList.innerHTML = WaveList.join("<br>")
+}
+
+function SetRouteData(){
+    Waves = document.getElementById("WaveSelect").value
+    WaveLength = document.getElementById("WaveLength").value
+    WaveInfo = []
+    WaveInfo.push(WaveLength)
+    for (let i = 1; i <= Waves; i++) {
+        WaveInfo.push(document.getElementById(`DSP${i}`).value)
+    }
+
+    netlifyIdentity.gotrue.currentUser().update({
+		data: {
+			RouteData: JSON.stringify(WaveInfo)
+		}
+	})
 }
