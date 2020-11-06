686a5b0afa6c1e7a840b8a73802dc76444dbd6d3
Lamasusb000 <Usbaldolamasjr@gmail.com>
2020-11-06 13:40:47 -0800

    Update Dispatcher.js

--- JavascriptFiles/Dispatcher.js
+++ JavascriptFiles/Dispatcher.js
@@ -1,5 +1,6 @@
 document.getElementById("WaveSelect").addEventListener(onchange, SetCount())
 
 function SetCount(){
-
+    Waves = document.getElementById("WaveSelect").value
+    console.log(`Waves starting at Zero ${Waves}`)
 }
