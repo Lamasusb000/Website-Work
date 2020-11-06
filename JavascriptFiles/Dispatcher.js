c5126242aa6442da5ddcb9292d2bf70cdc4f1f35
Lamasusb000 <Usbaldolamasjr@gmail.com>
2020-11-06 13:46:15 -0800

    Formatting

--- JavascriptFiles/Dispatcher.js
+++ JavascriptFiles/Dispatcher.js
@@ -1,6 +1,4 @@
-document.getElementById("WaveSelect").addEventListener(onchange, SetCount())
-
 function SetCount(){
     Waves = document.getElementById("WaveSelect").value
-    console.log(`Waves starting at Zero ${Waves}`)
+    console.log(`${Waves} Waves`)
 }
